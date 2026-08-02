4e83dc242a09a2e01c72234b414c0203f2f91e3d
sfx <fulmare90@gmail.com>
2026-08-02 19:53:31 +0200

    more robust screen tests

--- packages/fmenu-display-test/package.nix
+++ packages/fmenu-display-test/package.nix
@@ -15,13 +15,16 @@
             | join("x")
             ')"
         wdotool mousemove --relative 12000 12000
-        ffplay -loglevel quiet -fs -crf 0 -f lavfi -autoexit -i "
-            color=c=#ffffff:d=25:s=$DISPLAY_SIZE,sendcmd='
-          5 color c #ff0000;
-         10 color c #00ff00;
-         15 color c #0000ff;
-         20 color c #000000
-        '"
+        ffplay -loglevel quiet -fs -autoexit -f lavfi -i "
+          color=c=white:s=$DISPLAY_SIZE:d=5[w];
+          color=c=red:s=$DISPLAY_SIZE:d=5[r];
+          color=c=green:s=$DISPLAY_SIZE:d=5[g];
+          color=c=blue:s=$DISPLAY_SIZE:d=5[b];
+          color=c=black:s=$DISPLAY_SIZE:d=5[k];
+          smptehdbars=s=$DISPLAY_SIZE:d=10[bars];
+          testsrc2=s=$DISPLAY_SIZE:d=10[mov];
+          [w][r][g][b][k][bars][mov]concat=n=7:v=1:a=0"
+
         wdotool mousemove --relative -100 -100
       '';
     };
